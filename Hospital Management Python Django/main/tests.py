0dda7514100f90ef8c823ccd06e48a01b9b8810d
Osama Aburideh <x00154068@mytudublin.ie>
2022-05-08 19:08:20 +0100

    heroku

--- Hospital Management Python Django/main/tests.py
+++ Hospital Management Python Django/main/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 
-from django.test import TestCase
-from django.contrib.auth.models import User
-from core.restaurant.forms import DeliveriesCreateForm
-from django.urls import reverse, resolve
-from core.restaurant import views
-from core.views import home
-from core.restaurant.views import profilepage
-from finalproject.urls import driver_urlpatterns
-
-
-# testing if a user is created
-
-
+# Create your tests here.
